ed6ee801d5a70c851ea3f1c30aa503757122bad5
MateoGrifsha <mateogrifsha1@gmail.com>
2023-10-12 19:15:07 +0200

    first commit

---
+++ backend/routes/workouts.js
@@ -0,0 +1,32 @@
+const express = require('express'); //requires express package
+
+const router = express.Router(); //creates an instance of the router
+
+//Gets all workouts
+router.get('/', (req, res) => {  //get data from router
+res.json({message: "GET all workouts"});
+});
+
+
+//Gets a single workout
+router.get('/:id', (req,res) => {;
+    res.json({message : 'GET a single workout'})
+})
+
+//POST a new workout
+router.post('/', (req, res) =>{
+    res.json({message: 'POST a new workout'})
+})
+
+//DELETE a new workout
+router.delete('/:id', (req, res) =>{
+    res.json({message: 'DELETE a workout'});
+});
+
+//UPDATE a new workout
+router.patch('/:id', (req, res) =>{
+    res.json({message: 'UPDATE a workout'});
+});
+
+
+module.exports = router; //export routes
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express'); //requires the package
+const mongoose = require('mongoose'); //requires mongoose
+
+//connect to database
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=> {
+        
+        //listen for requests
+        app.listen(process.env.PORT, () =>{
+            console.log('connected to databse & listening on port 4000');
+        }); //1st param: port number from .env file
+    })
+        .catch((error)=>{
+        console.log(error); //catches error if the uri or password is not okay
+    })
+
+const app = express(); //creates express app
+const workoutRoutes = require('./routes/workouts');
+
+//middleware
+app.use(express.json())
+app.use((req, res, next)=>{
+    console.log(req.path, req.method); //returns the path, for example '/' and the method for example POST
+    next(); 
+});
+
+//routes
+app.use('/api/workouts', workoutRoutes) //when we send req to the url it uses the workoutRoutes
+
+
+
+
