81d9ed387b28b8bec28cff8eff08094a892ed349
MateoGrifsha <mateogrifsha1@gmail.com>
2023-10-19 18:31:58 +0200

    added delete

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,10 +1,28 @@
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+
 const WorkoutDetails = ({workout}) =>{
+    const { dispatch } = useWorkoutsContext()
+    
+    const handleClick = async () => {
+        const response = await fetch('/api/workouts/' + workout._id, {
+            method: 'DELETE'
+        })
+        const json = await response.json()
+
+        if(response.ok){
+            dispatch({
+                type: 'DELETE_WORKOUT', payload : jsonc 
+            })
+        }
+    }
+
     return(
         <div className="workout-details">
             <h4>{workout.title}</h4>
             <p><strong>Load (kg): {workout.load}</strong></p>
             <p><strong>Reps : {workout.reps}</strong></p>
             <p><strong>{workout.createdAt}</strong></p>
+            <span onClick ={handleClick}>Delete</span>
 
         </div>
     )
